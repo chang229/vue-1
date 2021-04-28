4c51fe05fee8cd56a435e3e54e7a5a761dde995d
changhongda <changhongda@111.com.cn>
2021-04-28 15:45:08 +0800

    源码阅读

--- src/platforms/web/util/index.js
+++ src/platforms/web/util/index.js
@@ -1,25 +1,30 @@
 /* @flow */
 
-import { warn } from 'core/util/index'
+import { warn } from "core/util/index";
 
-export * from './attrs'
-export * from './class'
-export * from './element'
+export * from "./attrs";
+export * from "./class";
+export * from "./element";
 
 /**
  * Query an element selector if it's not an element already.
  */
-export function query (el: string | Element): Element {
-  if (typeof el === 'string') {
-    const selected = document.querySelector(el)
+export function query(el: string | Element): Element {
+  //判断传入的el是不是字符串
+  if (typeof el === "string") {
+    // 获取el元素
+    const selected = document.querySelector(el);
     if (!selected) {
-      process.env.NODE_ENV !== 'production' && warn(
-        'Cannot find element: ' + el
-      )
-      return document.createElement('div')
+      // 页面上没有el元素，开发环境下报错
+      process.env.NODE_ENV !== "production" &&
+        warn("Cannot find element: " + el);
+      // 生产环境下创建一个div元素并返回
+      return document.createElement("div");
     }
-    return selected
+    // 页面上有el元素直接返回
+    return selected;
   } else {
-    return el
+    // el不是字符串直接返回
+    return el;
   }
 }
